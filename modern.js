8040e48e81d7512cbf8ce9bf1fd23d4b45b6d17c
Volker E <volker.e@wikimedia.org>
2021-05-14 16:26:26 -0700

    Replace single config file with separate files
    Former is only working with env variables and is not practical.
    Also remove MobileFrontend specific config, that's simpler done by
      extends browserslist-config-wikimedia/basic
      not IE <= 10

---
+++ modern.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = [
+	'last 2 Chrome versions',
+	'last 2 Firefox versions',
+	'last 2 Opera versions',
+	'last 2 Edge versions',
+	'IE >= 11',
+	'Safari >= 9.1',
+	'iOS >= 9',
+	'Android >= 4.3'
+];
