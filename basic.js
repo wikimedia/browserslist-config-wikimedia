93d3743fdd099a936ff248cdb16b3d23194d5090
James D. Forrester <jforrester@wikimedia.org>
2022-04-20 08:30:48 -0400

    basic: Bump requirements for 2022
    * Internet Explorer	9-10	→ 11
    * Firefox		27+	→ 39+
    * Android		4.3+	→ 5+

--- basic.js
+++ basic.js
@@ -2,11 +2,11 @@
 
 module.exports = [
 	'Chrome >= 31',
-	'Firefox >= 27',
+	'Firefox >= 39',
 	'Opera >= 18',
 	'Edge >= 12',
-	'IE >= 9',
+	'IE >= 11',
 	'Safari >= 9.1',
 	'iOS >= 9',
-	'Android >= 4.3'
+	'Android >= 5'
 ];
